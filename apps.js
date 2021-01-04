700982d3b533c41303934fcdcfda2401358dc557
phoenixbyrd <james.gundlach79@gmail.com>
2021-01-03 21:02:18 -0600

    initial commit

---
+++ apps.js
@@ -0,0 +1,214 @@
+function openBflix() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "http://bflix.to";
+}
+
+function openYoutube() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "http://youtube.com";
+}
+
+function openMessenger() {
+  myWindow = window.open("", "", "width=500, height=800");
+  myWindow.location.href = "http://messenger.com";
+}
+
+function openReddit() {
+  myWindow = window.open("", "", "width=600, height=900");
+  myWindow.location.href = "http://reddit.com";
+}
+
+function openAmongUS() {
+  myWindow = window.open("", "", "width=1200, height=900");
+  myWindow.location.href = "http://titotu.io/embed/among-us-freeplay";
+}
+
+function openEntanglement() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "http://entanglement.gopherwoodstudios.com/en-US-index.html";
+}
+
+function openKiwiIRC() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://kiwiirc.com/nextclient/";
+}
+
+function openDiablo() {
+  myWindow = window.open("", "", "width=800, height=600");
+  myWindow.location.href = "https://d07riv.github.io/diabloweb/";
+}
+
+function openEntanglement() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "http://entanglement.gopherwoodstudios.com/en-US-index.html";
+}
+
+function openCandyBubble() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://play.famobi.com/candy-bubble";
+}
+
+function openLara() {
+  myWindow = window.open("", "", "width=850, height=550");
+  myWindow.location.href = "http://xproger.info/projects/OpenLara/";
+}
+
+function openNetflix() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "https://www.netflix.com/browse";
+}
+
+function openHulu() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "https://www.hulu.com/hub/home";
+}
+
+function openPluto() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "https://pluto.tv/live-tv";
+}
+
+function openSpotify() {
+  myWindow = window.open("", "", "width=1200, height=600");
+  myWindow.location.href = "https://open.spotify.com/";
+}
+
+function openRedax() {
+  myWindow = window.open("", "", "width=600, height=850");
+  myWindow.location.href = "https://redaxsounds.netlify.app/";
+}
+
+function openSWRadio() {
+    myWindow = window.open("", "", "width=1000, height=550");
+    myWindow.location.href = "https://www.chilton.com/R8/receiver.html";
+}
+
+function openTwitter() {
+    myWindow = window.open("", "", "width=600, height=800");
+    myWindow.location.href = "https://twitter.com/home";
+}
+
+function openBQuest() {
+    myWindow = window.open("", "", "width=1200, height=768");
+    myWindow.location.href = "http://browserquest.herokuapp.com/";
+}
+
+function openEmpire() {
+    myWindow = window.open("", "", "width=1500, height=900");
+    myWindow.location.href = "https://empire.goodgamestudios.com/";
+}
+
+function openFallBros() {
+    myWindow = window.open("", "", "width=1500, height=900");
+    myWindow.location.href = "http://titotu.io/embed/fall-bros";
+}
+
+function openKrunker() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "https://krunker.io/";
+}
+
+function openMahjong() {
+  myWindow = window.open("", "", "width=1200, height=900");
+  myWindow.location.href = "https://play.famobi.com/mahjong-3d";
+}
+
+function openMars() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "https://play.mars-tomorrow.com/";
+}
+
+function openMinecraft() {
+  myWindow = window.open("", "", "width=1024, height=768");
+  myWindow.location.href = "https://classic.minecraft.net/";
+}
+
+function openMiniroyale() {
+  myWindow = window.open("", "", "width=1200, height=800");
+  myWindow.location.href = "https://miniroyale2.io/";
+}
+
+function openSkribbl() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://skribbl.io/";
+}
+
+function openTreasureArena() {
+  myWindow = window.open("", "", "width=1024, height=768");
+  myWindow.location.href = "http://titotu.io/embed/treasurearena-com";
+}
+
+function openVaporboy() {
+  myWindow = window.open("", "", "width=1000, height=800");
+  myWindow.location.href = "https://vaporboy.net/";
+}
+
+function openAnime() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "https://animesuge.io/";
+}
+
+function openDiscord() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://discord.com/login";
+}
+
+function openElement() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://app.element.io/";
+}
+
+function openInstagram() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://www.instagram.com/";
+}
+
+function openTeams() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://teams.microsoft.com/";
+}
+
+function openSkype() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://web.skype.com/";
+}
+
+function openTelegram() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://web.telegram.org/";
+}
+
+function openTiktok() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://www.tiktok.com/foryou/";
+}
+
+function openWhat() {
+  myWindow = window.open("", "", "width=1000, height=768");
+  myWindow.location.href = "https://web.whatsapp.com/";
+}
+
+function openBandlab() {
+  myWindow = window.open("", "", "width=1200, height=768");
+  myWindow.location.href = "https://www.bandlab.com/feed";
+}
+
+function openGmail() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "https://mail.google.com/mail/u/0/#inbox";
+}
+
+function openTranslate() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://translate.google.com/";
+}
+
+function openOutlook() {
+  myWindow = window.open("", "", "width=1500, height=900");
+  myWindow.location.href = "https://outlook.office.com/mail/";
+}
+
+function openZoom() {
+  myWindow = window.open("", "", "width=600, height=800");
+  myWindow.location.href = "https://us04web.zoom.us/";
+}
