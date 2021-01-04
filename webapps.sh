10b77c0ac8f749bbe272c57d4d8cea7a75985de2
phoenixbyrd <james.gundlach79@gmail.com>
2021-01-04 07:11:55 -0600

    webapps.sh rm cache

--- webapps.sh
+++ webapps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -d /home/pi/.webapps/webapps/Default/Cache ]
 then
-    mv /home/pi/.webapps/webapps/Default/Cache /home/pi/.webapps/webapps/Default/Cache_backup
+    rm -r /home/pi/.webapps/webapps/Default/Cache
 fi
 chromium-browser %U --user-agent="Mozilla/5.0 (X11; CrOS armv7l 11895.95.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.125 Safari/537.36" --user-data-dir=/home/pi/.webapps/webapps --app=https://phoenixbyrd.github.io/TwisterWebApps/games.html  
