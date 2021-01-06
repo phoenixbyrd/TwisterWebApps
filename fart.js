c6e2788170cf12c368edf5f9dbd0e695bd5b9096
phoenixbyrd <james.gundlach79@gmail.com>
2021-01-06 13:27:38 -0600

    fart.html

---
+++ fart.js
@@ -0,0 +1,12 @@
+function playBold() {
+    var audio = document.getElementById("bold");
+    audio.play();
+  }
+  function playWet() {
+    var audio = document.getElementById("wet");
+    audio.play();
+  }
+  function playWet2() {
+    var audio = document.getElementById("wet2");
+    audio.play();
+  }
